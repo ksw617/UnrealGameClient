413423a11a866671ca20ab77f5c6523fd7fbcd4a
lecture <ksw617@gmail.com>
2024-12-02 21:04:05 +0900

    update

---
+++ Source/UnrealGameClient/NetworkGameInstance.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "NetworkGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALGAMECLIENT_API UNetworkGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+public:
+	class FSocket* Socket = nullptr;
+	FString IpAddress = TEXT("127.0.0.1");
+	int16 Port = 27015;
+public:
+	UFUNCTION(BlueprintCallable)
+	void ConnectToServer();
+	UFUNCTION(BlueprintCallable)
+	void DisconnectServer();
+
+};
